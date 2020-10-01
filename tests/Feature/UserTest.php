a61b2d72b90ac163e8bb09459fe4f469e88e856c
Deepansh Sachdeva <deepansh.99530@gmail.com>
2020-10-01 01:24:06 -0400

    :wrench: modify test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -18,10 +18,10 @@ class UserTest extends TestCase
     public function testCreateUser()
     {
         $response = $this->json('POST', route('user.create'), [
-            'first_name'    => 'Testing',
-            'last_name'     => 'User',
-            'email_address' => 'testinguser@mail.com',
-            'password'      => 'P@ssw0rd123'
+            'first_name' => 'Testing',
+            'last_name'  => 'User',
+            'username'   => 'testinguser@mail.com',
+            'password'   => 'P@ssw0rd123'
         ]);
 
         $response->assertStatus(201);
